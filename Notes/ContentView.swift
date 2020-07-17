17b1422a0348dc8c63b900bbdfc75efc6649db0a
Krishna Nanduri <krishna.nanduri@icloud.com>
2020-07-17 17:47:50 +0530

    Initial Commit

---
+++ Notes/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Notes
+//
+//  Created by Krishna Nanduri on 17/07/20.
+//  Copyright © 2020 Krishna Nanduri. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
